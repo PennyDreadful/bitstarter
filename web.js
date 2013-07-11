3a07678d1e0ae8eff408bc5cc9eeb060c265982a
PennyDreadful <o.eytonwilliams@gmail.com>
2013-07-11 15:42:23 +0000

    Debugging web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    Buffer html = fs.readFileSync("index.html");
+    var html = fs.readFileSync("index.html");
     response.send(html.toString());
+
 //  response.send('Hello World 2!');
 });
 
